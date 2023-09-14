188ac47f285040a1867e1e0ee8dfd537b13c23c1
Julian Uszkoreit <julian.uszkoreit@rub.de>
2023-09-14 11:34:09 +0200

    Update abstract_converter.py to make CLI work
    The cls must be given (at least in my setup of Python)

--- src/sdrf_convert/abstract_converter.py
+++ src/sdrf_convert/abstract_converter.py
@@ -299,7 +299,7 @@ class AbstractConverter:
         raise NotImplementedError(f"Method convert() not implemented for {self.__name__}")
     
     @classmethod
-    def convert_via_cli(cli_args: argparse.Namespace):
+    def convert_via_cli(cls, cli_args: argparse.Namespace):
         """Uses the CLI arguments convert a SDRF file to the tool config.
         Example for an implemented subclass:
         ```python
