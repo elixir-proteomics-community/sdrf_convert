b41caf533a2a69dcf2d39426b33ceaafd04ef3c7
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2023-09-15 17:44:03 +0200

    fix wrong variable

--- src/sdrf_convert/abstract_converter.py
+++ src/sdrf_convert/abstract_converter.py
@@ -241,7 +241,7 @@ class AbstractConverter:
             The converted SDRF file
         """
         self.init_converter(sdrf)
-        raise NotImplementedError(f"Method convert() not implemented for {cls.__name__}")
+        raise NotImplementedError(f"Method convert() not implemented for {self.__class__.__name__}")
     
     @classmethod
     def convert_via_cli(cls, cli_args: argparse.Namespace):
