66a66faa90a299e6cc39357fe9722d1bcfd62978
Uladzislau Vadadokhau <vadadokhau@mailbox.unideb.hu>
2023-09-21 17:04:39 +0200

    added mass tolerance and missed cleavages parsing

--- src/sdrf_convert/diann_converter.py
+++ src/sdrf_convert/diann_converter.py
@@ -1,5 +1,5 @@
 """
-Module for converting SDRF filres to DIA-NN CLI call
+Module for converting SDRF files to DIA-NN CLI call
 """
 import os
 from typing import ClassVar, Dict, List, Type, Union, Any
@@ -29,12 +29,13 @@ class DiannConverter(AbstractConverter):
         "comment[cleavage agent details]": [pd.StringDtype()]
     }
 
+
     OPTIONAL_COLUMN_PROPERTIES: ClassVar[Dict[str, List[Type]]] = {
         "comment[number of missed cleavages]": [pd.Int64Dtype()]
     }
 
     SDRF_COL_NAME_DIANN_PARAM_MAP: ClassVar[Dict[str, List[str]]] = {
-        "comment[number of missed cleavages]": ["comment[modification parameters]*"],
+        "comment[number of missed cleavages]": ["--missed-cleavages"],
         "comment[modification parameters]": ["--fixed-mod", "--var-mod"],
         "comment[precursor mass tolerance]": ["--mass-acc-ms1"],
         "comment[fragment mass tolerance]": ["--mass-acc"],
@@ -43,13 +44,13 @@ class DiannConverter(AbstractConverter):
 
     }
 
-    PROTESES_MAP: ClassVar[Dict[str, str]] = {
+    CLEAVAGE_SITES_MAP: ClassVar[Dict[str, str]] = {
         "Trypsin/P": "K*,R*",
         "Trypsin": "K*,R*,!*P",
-        "Lys-C": "(?<=K)(?!P)",
-        "Chymotrypsin": "",
-        "Asp-N": "",
-        "Glu-C": ""
+        "Lys-C": "K*",
+        "Chymotrypsin": "F*,L*,M*,W*,Y*,!*P",
+        "Asp-N": "*D",
+        "Glu-C": "E*"
 
     }
 
@@ -62,18 +63,54 @@ class DiannConverter(AbstractConverter):
         -------
         str
             String with parsed names of MS file(s)
-            e.g. --f folder/subfolder/file_1.raw
+            e.g. "--f folder/subfolder/file_1.raw"
         """
         strinf = ''
 
         file_names = sdrf_df['comment[data file]'].values
         #apply(lambda x: strinf + '--f ' + path_to_files + '/' + x).to_string()
 
-        strinf = ''
         for name in file_names:
             strinf = strinf + ' --f ' + self.raw_files_path + '/' + name
 
         return strinf
+    
+    def parse_diann_params(self, sdrf: pd.DataFrame, opt_columns: set) -> str:
+        """
+        Takes values from OPTIONAL and REQUIRED columns
+        in SDRF (except data file) and parse them into
+        DIA-NN format according to SDRF_COL_NAME_DIANN_PARAM_MAP.
+
+        Returns
+        -------
+        str
+            String with parsed parameters for DIA-NN
+            taken from SDRF REQUIRED and OPTIONAL columns
+            e.g. "--var-mod UniMod:1,42.010565,*n --cut K*,R*,!*P --mass-acc-ms1 0"
+        """
+        # parse precursor and fragment mass tolerance
+        prec_mass_tol = sdrf['comment[precursor mass tolerance]'].values[0]
+        frag_mass_tol = sdrf['comment[fragment mass tolerance]'].values[0]
+        mass_tol_str = self.SDRF_COL_NAME_DIANN_PARAM_MAP['comment[precursor mass tolerance]'][0] + ' ' + str(prec_mass_tol) + ' ' +\
+                            self.SDRF_COL_NAME_DIANN_PARAM_MAP['comment[fragment mass tolerance]'][0] + ' ' + str(frag_mass_tol) + ' '
+        
+        # parse modifications
+        mod_str = ''
+
+        # parse proteases
+        clvg_ag_str = ''
+
+        # parse missed cleavage
+        if len(opt_columns) > 0: 
+            missed_cleavages = sdrf['comment[number of missed cleavages]'].values[0]
+            missed_cleavages_str = self.SDRF_COL_NAME_DIANN_PARAM_MAP['comment[number of missed cleavages]'][0] + ' ' + str(missed_cleavages)
+
+        else:
+            missed_cleavages_str = ''
+        
+        return mass_tol_str + mod_str + clvg_ag_str + missed_cleavages_str
+
+
 
     def convert(self, sdrf: Union[pd.DataFrame, IOBase, Path]) -> Any:
         """
@@ -87,4 +124,9 @@ class DiannConverter(AbstractConverter):
             The converted SDRF file
         """
         self.init_converter(sdrf)
-        return str(self.diann_path) + ' ' + self.parse_file_names(self.sdrf_df) + ' ' + self.diann_params
+        return str(self.diann_path) + ' ' \
+            + self.parse_file_names(self.sdrf_df) + ' '\
+                  + self.diann_params + ' ' +\
+                        self.parse_diann_params(self.sdrf_df, self.present_optional_columns)
+                       #self.sdrf_df['comment[cleavage agent details]'].values[0] +\
+                            #self.CLEAVAGE_SITES_MAP[self.ontology_str_to_dict(self.sdrf_df['comment[cleavage agent details]'].values[0])['NT']]
